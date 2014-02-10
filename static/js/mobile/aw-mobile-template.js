ea8a3067b320a4ff22e738ad1babea2be866526b
joojler <tj1ster@gmail.com>
2014-02-10 22:34:28 +0800

    Upgrade to 2.5 release

--- static/js/mobile/aw-mobile-template.js
+++ static/js/mobile/aw-mobile-template.js
@@ -71,6 +71,7 @@ var AW_MOBILE_TEMPLATE = {
 		   '<div class="modal-dialog">'+
 				'<div class="modal-content">'+
 				   '<form id="publish" action="' + G_BASE_URL + '/inbox/ajax/send/" method="post" id="quick_publish" onsubmit="return false">'+
+				   		'<input type="hidden" name="post_hash" value="' + G_POST_HASH + '" />'+
 				    	'<div class="modal-header">'+
 					    	'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>'+
 					    	'<h3 class="modal-title" id="myModalLabel">' + _t('发送私信') + '</h3>'+
@@ -128,5 +129,29 @@ var AW_MOBILE_TEMPLATE = {
 			'{{#items}}'+
 				'<li><a data-value="{{id}}">{{title}}</a></li>'+
 			'{{/items}}'+
-		'</ul>'
+		'</ul>',
+	'editCommentBox' :
+		'<div class="modal fade alert-commentEdit">'+
+			'<div class="modal-dialog">'+
+				'<div class="modal-content">'+
+					'<div class="modal-header">'+
+						'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>'+
+						'<h3 class="modal-title" id="myModalLabel">' + _t('编辑回复') + '</h3>'+
+					'</div>'+
+					'<form action="' + G_BASE_URL + '/question/ajax/update_answer/answer_id-{{answer_id}}" method="post" onsubmit="return false" id="answer_edit">'+
+					'<div class="modal-body">'+
+						'<input type="hidden" name="attach_access_key" value="{{attach_access_key}}" />'+
+						'<textarea name="answer_content" id="editor_reply" class="form-control textarea_content" rows="5"></textarea>'+
+						'<div class="aw-file-upload-box">'+
+							'<span id="file_uploader_answer_edit"></span>'+
+						'</div>'+
+					'</div>'+
+					'<div class="modal-footer">'+
+						'<span><input id="aw-do-delete" type="checkbox" value="1" name="do_delete" /><label for="aw-do-delete">&nbsp;' + _t('删除回复') + '</label></span>'+
+						'<button class="btn btn-large btn-success" onclick="ajax_post($(\'#answer_edit\'), _ajax_post_alert_processer);return false;">' + _t('确定') + '</button>'+
+					'</div>'+
+					'</form>'+
+				'</div>'+
+			'</div>'+
+		'</div>'
 }
